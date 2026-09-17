05532ca138a83271e2fe3e54dee9bb1a056913f4
michalbelicka <michal.belicka2310@gmail.com>
2026-09-17 12:15:55 +0200

    Add signup details

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -6,4 +6,15 @@ class SignupPage:
 
     def click_signup_login(self):
         self.page.get_by_role("link", name="Signup / Login").click()
-    
+
+    def is_signup_section_visible(self):
+        return self.page.get_by_role("heading", name="New User Signup!").is_visible()
+
+    def enter_signup_details(self, name, email):
+        self.page.get_by_placeholder("name").fill(name)
+
+        signup_form = self.page.locator(".signup-form")
+        signup_form.get_by_placeholder("Email Address").fill(email)
+
+
+        
--- tests/test_signup_and_delete_account.py
+++ tests/test_signup_and_delete_account.py
@@ -1,8 +1,11 @@
 from pages.signup_page import SignupPage
 
-def test_signup(home_page, page):
+def test_signup_and_delete_account(home_page, page):
 
     signup_page = SignupPage(page)
     
     signup_page.click_signup_login()
-    
+
+    assert signup_page.is_signup_section_visible()
+
+    signup_page.enter_signup_details("Tester", "tester@email.com")

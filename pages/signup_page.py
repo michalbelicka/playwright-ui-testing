500c521cc1f3c12f5517f2895bf5d389efcb85cb
michalbelicka <michal.belicka2310@gmail.com>
2026-09-18 12:16:56 +0200

    Update sign up test and page

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -16,5 +16,23 @@ class SignupPage:
         signup_form = self.page.locator(".signup-form")
         signup_form.get_by_placeholder("Email Address").fill(email)
 
+    def click_signup(self):
+        self.page.get_by_role("button", name="Signup").click()
 
-        
+    def account_information_heading(self):
+        return self.page.locator(".title.text-center", has_text="Enter Account Information")
+
+    def select_title(self, title):
+        self.page.locator(f'input[type="radio"][value={title}]').check()
+
+    def select_radio(self, title):
+        return self.page.locator(f'input[type="radio"][value={title}]')
+
+    def name_input(self):
+        return self.page.locator('[data-qa="name"]')
+
+    def email_input(self):
+        return self.page.locator('[data-qa="email"]')
+
+    def password_input(self, password):
+        self.page.locator('[data-qa="password"]').fill(password)
--- tests/test_signup_and_delete_account.py
+++ tests/test_signup_and_delete_account.py
@@ -1,4 +1,6 @@
 from pages.signup_page import SignupPage
+import random
+from playwright.sync_api import expect
 
 def test_signup_and_delete_account(home_page, page):
 
@@ -8,4 +10,23 @@ def test_signup_and_delete_account(home_page, page):
 
     assert signup_page.is_signup_section_visible()
 
-    signup_page.enter_signup_details("Tester", "tester@email.com")
+    name = f"Tester{random.randint(1000, 9999)}"
+    email = f"tester{random.randint(1000, 9999)}@example.com"
+    password = "TestPassword123!"
+
+    signup_page.enter_signup_details(name, email)
+
+    signup_page.click_signup()
+
+    expect(signup_page.account_information_heading()).to_be_visible()
+
+    signup_page.select_title("Mr")
+
+    expect(signup_page.select_radio("Mr")).to_be_checked()
+
+    expect(signup_page.name_input()).to_have_value(name) == name
+
+    expect(signup_page.email_input()).to_have_value(email) == email
+
+    signup_page.password_input(password)
+

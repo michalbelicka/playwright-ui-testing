0e8ac6b55c42ca1543844c6c1beafbdd7d5c6e40
michalbelicka <michal.belicka2310@gmail.com>
2026-09-23 16:19:11 +0200

    Add login page and test

---
+++ pages/login_page.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Page
+
+class LoginPage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def enter_login_details(self, email, password):
+        self.page.locator('[data-qa="login-email"]').fill(email)
+        self.page.get_by_placeholder("Password").fill(password)
+
+    def login_section(self):
+        return self.page.get_by_role("heading", name="Login to your account")
+
+    def click_login(self):
+        self.page.get_by_role("button", name="Login").click()
---
+++ tests/UI/test_login.py
@@ -0,0 +1,24 @@
+from pages.signup_page import SignupPage
+from pages.login_page import LoginPage
+from playwright.sync_api import expect
+
+def test_login(home_page):
+
+    signup_page = SignupPage(home_page)
+    login_page = LoginPage(home_page)
+
+    signup_page.click_signup_login()
+
+    expect(login_page.login_section()).to_be_visible()
+
+    login_page.enter_login_details("tester2310@example.com", "password")
+
+    login_page.click_login()
+
+    name = "Tester2310"
+    expected_text = f"Logged in as {name}"
+
+    expect(signup_page.logged_in_as()).to_have_text(expected_text)
+
+
+
